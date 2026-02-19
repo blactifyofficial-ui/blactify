62626f314715ffcc7d2ca628392b462990043cc4
NITHIN-NT <nithinraj07sachu@gmail.com>
2026-02-19 17:16:27 +0530

    chore: add Organization and WebSite JSON-LD to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,5 +25,41 @@ async function getInitialProducts() {
 export default async function Page() {
   const products = await getInitialProducts();
 
-  return <HomeClient initialProducts={products} />;
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Blactify",
+    "url": "https://blactify.com",
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://blactify.com/shop?q={search_term_string}",
+      "query-input": "required name=search_term_string"
+    }
+  };
+
+  const orgJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Blactify",
+    "url": "https://blactify.com",
+    "logo": "https://blactify.com/logo-v1.png",
+    "sameAs": [
+      "https://twitter.com/blactify",
+      "https://instagram.com/blactify"
+    ]
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }}
+      />
+      <HomeClient initialProducts={products} />
+    </>
+  );
 }
